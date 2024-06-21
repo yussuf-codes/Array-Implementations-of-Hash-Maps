585cba6e6d039763cb08c46b349aeb0724c35c39
yussuf-codes <yussuf.code@gmail.com>
2024-06-21 17:02:01 +0300

    Project skeleton

---
+++ HashMap/Implementations/OpenAddressing.cs
@@ -0,0 +1,36 @@
+using System;
+using HashMap.Interfaces;
+
+namespace HashMap.Implementations.OpenAddressing;
+
+public class HashMap<Key, Value> : IHashMap<Key, Value>
+{
+    private int[] associative_array;
+
+    private int associative_array_length;
+
+    public void Add(Key key, Value value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Value Get(Key key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Key[] Keys()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(Key key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Key key, Value value)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ HashMap/Interfaces/IHashMap.cs
@@ -0,0 +1,10 @@
+﻿namespace HashMap.Interfaces;
+
+public interface IHashMap<Key, Value>
+{
+    void Add(Key key, Value value);
+    Value Get(Key key);
+    Key[] Keys();
+    void Remove(Key key);
+    void Update(Key key, Value value);
+}
