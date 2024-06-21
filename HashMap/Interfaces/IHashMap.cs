9bb36302be9b83117d0f71a97f0a00dbe9bb9ed0
yussuf-codes <yussuf.code@gmail.com>
2024-06-21 18:32:06 +0300

    Add time complexity

--- HashMap/Interfaces/IHashMap.cs
+++ HashMap/Interfaces/IHashMap.cs
@@ -2,9 +2,18 @@
 
 public interface IHashMap<Key, Value>
 {
+    // Time Complexity: O(1)
     void Add(Key key, Value value);
+
+    // Time Complexity: O(1)
     Value Get(Key key);
+
+    // Time Complexity: O(n)
     Key[] Keys();
+
+    // Time Complexity: O(1)
     void Remove(Key key);
+
+    // Time Complexity: O(1)
     void Update(Key key, Value value);
 }
